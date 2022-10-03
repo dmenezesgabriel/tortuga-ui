42abf868067eedaf6c325c3072ec556dfbdcfd77
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-10-03 17:59:53 -0300

    Add scroll cue history

--- src/components/animations/docs/ScrollCue.story.vue
+++ src/components/animations/docs/ScrollCue.story.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import ScrollCue from '@/components/animations/ScrollCue';
+
+function initState() {
+    return {
+        color: "black",
+    }
+}
 </script>
 
 <template>
-    <Story group="animations">
-        <div class="absolute-center">
-            <ScrollCue />
-        </div>
+    <Story
+        group="animations"
+        :init-state="initState"
+    >
+        <template #default="{state}">
+            <div class="absolute-center">
+                <ScrollCue :color="state.color" />
+            </div>
+        </template>
     </Story>
 </template>
