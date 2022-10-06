2b203f1e41ddf52b2a6dd8f5daa05da28921f4fb
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-10-06 20:58:57 -0300

    Add click action on button call to action story

--- src/components/ButtonCallToAction/docs/ButtonCallToAction.stories.ts
+++ src/components/ButtonCallToAction/docs/ButtonCallToAction.stories.ts
@@ -1,18 +1,26 @@
 import ButtonCallToAction from "@/components/ButtonCallToAction/ButtonCallToAction";
 import type { Story } from "@storybook/vue3";
+import { action } from "@storybook/addon-actions";
 
 export default {
   title: "Components/Buttons/ButtonCallToAction",
   component: ButtonCallToAction,
+  argTypes: {
+    onClick: {},
+  },
   decorators: [
     () => ({ template: "<div style='margin: 3em'><story /></div>" }),
   ],
 };
 
+export const actionsData = {
+  onClick: action("click"),
+};
+
 const Template = (args: any) => ({
   components: { ButtonCallToAction },
   setup() {
-    return { args };
+    return { args, ...actionsData };
   },
   template: "<ButtonCallToAction v-bind='args' />",
 });
