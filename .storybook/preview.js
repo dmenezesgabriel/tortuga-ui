71e65c68e565492e69e6129892047656308e57f8
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-10-08 17:34:45 -0300

    FIx theme overriding

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,16 @@
 import "../src/assets/scss/index.scss";
+import "bootstrap/js/dist/alert";
+import "bootstrap/js/dist/base-component";
+import "bootstrap/js/dist/button";
+// import "bootstrap/js/dist/carousel";
+import "bootstrap/js/dist/collapse";
+import "bootstrap/js/dist/dropdown";
+import "bootstrap/js/dist/modal";
+import "bootstrap/js/dist/offcanvas"; // sidebar, depends on modal
+import "bootstrap/js/dist/popover";
+// import "bootstrap/js/dist/scrollspy";
+// import "bootstrap/js/dist/tab";
+import "bootstrap/js/dist/toast";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
