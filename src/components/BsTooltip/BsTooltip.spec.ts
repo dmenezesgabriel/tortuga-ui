2efac5c13b856e601127976f9dd6fa804dda8525
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-10-17 11:47:44 -0300

    Add BsTooltip render tests

---
+++ src/components/BsTooltip/BsTooltip.spec.ts
@@ -0,0 +1,23 @@
+/**
+ * @vitest-environment jsdom
+ */
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import BsTooltip from "@/components/BsTooltip/BsTooltip.vue";
+
+describe("BsTooltip", async () => {
+  it("should render", async () => {
+    // Arrange
+    const wrapper = mount(BsTooltip, {
+      props: {
+        options: {
+          title: "This is a tooltip",
+        },
+      },
+    });
+
+    // Assert
+    expect(wrapper.find("span").exists()).toBeTruthy();
+    expect(wrapper.attributes("aria-label")).toContain("tooltip");
+  });
+});
