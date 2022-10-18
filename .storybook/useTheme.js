4228730049bb4b17418e66b2926a77f7de1262f9
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-10-18 18:37:05 -0300

    Document useTheme decorator

--- .storybook/useTheme.js
+++ .storybook/useTheme.js
@@ -1,9 +1,20 @@
+/**
+ * useTheme decorator for storybook component stories.
+ */
 import { useEffect, useGlobals } from "@storybook/addons";
 
 export const useTheme = (StoryFn) => {
+  /**
+   * @param {StoryFn} StoryFn
+   * @return {StoryFn}
+   */
+
+  // Get theme from global types declared at ./storybook/preview.js
   const [{ theme }] = useGlobals();
 
   useEffect(() => {
+    // Add data-mode attribute and theme class to Storybook's story iframe
+    // This classes are styled at src/assets/scss/
     document.body.removeAttribute("class");
     document.body.setAttribute("class", theme);
     document.documentElement.removeAttribute("data-mode");
