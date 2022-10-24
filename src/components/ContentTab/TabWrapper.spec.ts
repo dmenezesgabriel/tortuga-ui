e55a2ad9a0b1928e97eb890650dd5dca6b445b38
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-10-24 15:20:10 -0300

    Add Tab content tests

--- src/components/ContentTab/TabWrapper.spec.ts
+++ src/components/ContentTab/TabWrapper.spec.ts
@@ -5,7 +5,7 @@ import { describe, it, expect, beforeEach } from "vitest";
 import { mount, VueWrapper } from "@vue/test-utils";
 import TabsWrapper from "@/components/ContentTab/TabsWrapper.vue";
 import TabContent from "@/components/ContentTab/TabContent.vue";
-import { h } from "vue";
+import { h, nextTick } from "vue";
 
 let wrapper: VueWrapper;
 
@@ -43,5 +43,55 @@ describe("TabWrapper", async () => {
       .at(0);
 
     expect(tabWrapper?.exists());
+  });
+
+  it("should emit tab click", () => {
+    // Act
+    wrapper.find("li").trigger("click");
+
+    // Assert
+    expect(wrapper.emitted()).toHaveProperty("tab-click");
+
+    // Arrange
+    const clickEvent = wrapper.emitted("tab-click");
+
+    // Assert
+    expect(clickEvent?.[0]).toEqual(["Tab 1"]);
+  });
+
+  it("should change tab on click", async () => {
+    // Arrange
+    const buttonFirstTab = wrapper
+      .findAll("li")
+      .filter((node) => node.text().match("Tab 1"))
+      .at(0);
+
+    // Assert
+    expect(buttonFirstTab?.classes()).toContain("selected");
+
+    // Assert
+    expect(wrapper.text()).toContain("Tab 2");
+
+    // Arrange
+    const buttonSecondTab = wrapper
+      .findAll("li")
+      .filter((node) => node.text().match("Tab 2"))
+      .at(0);
+
+    // Act
+    buttonSecondTab?.trigger("click");
+    await nextTick();
+
+    // Assert
+    expect(buttonSecondTab?.classes()).toContain("selected");
+
+    // Arrange
+    const secondTab = wrapper
+      .findAll(".tab-content")
+      .filter((node) => node.text().match("Tab 2 content"))
+      .at(0);
+
+    // Act
+    expect(secondTab?.exists());
   });
 });
