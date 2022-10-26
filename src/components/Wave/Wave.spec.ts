66929ab2d513444e1fe828455136a465a8e9472d
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-10-26 11:16:15 -0300

    Add wave tests

---
+++ src/components/Wave/Wave.spec.ts
@@ -0,0 +1,30 @@
+/**
+ * @vitest-environment jsdom
+ */
+import { describe, it, expect, beforeEach } from "vitest";
+import { shallowMount, type VueWrapper } from "@vue/test-utils";
+import Wave from "@/components/Wave/Wave";
+
+let wrapper: VueWrapper;
+
+describe("Wave", () => {
+  beforeEach(() => {
+    // Arrange
+    wrapper = shallowMount(Wave as any, {
+      props: {
+        direction: "upRight",
+      },
+    });
+  });
+
+  it("is called", () => {
+    // Assert
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it("should render", () => {
+    // Assert
+    expect(wrapper.find("svg").exists()).toBeTruthy();
+    expect(wrapper.classes()).toContain("wave");
+  });
+});
