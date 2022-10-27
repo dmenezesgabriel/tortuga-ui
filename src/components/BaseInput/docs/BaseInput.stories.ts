cdd8e44cce6a40f394c080635cdb252ab1b4b01e
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-10-27 12:30:46 -0300

    Update base input docs

--- src/components/BaseInput/docs/BaseInput.stories.ts
+++ src/components/BaseInput/docs/BaseInput.stories.ts
@@ -23,7 +23,7 @@ const Template = (args: any) => ({
     return { args };
   },
   template: `
-    <BaseInput v-bind='args' type="text">
+    <BaseInput v-bind='args'>
       <template #label>
         <span class="mx-2">This is a Label: </span>
       </template>
@@ -32,7 +32,59 @@ const Template = (args: any) => ({
 });
 
 export const Default: Story = Template.bind({});
-Default.args = {};
+Default.args = {
+  type: "text",
+};
+
+export const TypeNumber: Story = Template.bind({});
+TypeNumber.args = {
+  type: "number",
+};
+
+export const TypeEmail: Story = Template.bind({});
+TypeEmail.args = {
+  type: "email",
+};
+
+export const TypePassword: Story = Template.bind({});
+TypePassword.args = {
+  type: "password",
+};
+
+export const TypeSearch: Story = Template.bind({});
+TypeSearch.args = {
+  type: "search",
+};
+
+export const TypeUrl: Story = Template.bind({});
+TypeUrl.args = {
+  type: "url",
+};
+
+export const TypeTel: Story = Template.bind({});
+TypeUrl.args = {
+  type: "tel",
+};
+
+export const TypeDate: Story = Template.bind({});
+TypeDate.args = {
+  type: "date",
+};
+
+export const TypeTime: Story = Template.bind({});
+TypeDate.args = {
+  type: "time",
+};
+
+export const TypeRange: Story = Template.bind({});
+TypeRange.args = {
+  type: "range",
+};
+
+export const TypeColor: Story = Template.bind({});
+TypeColor.args = {
+  type: "color",
+};
 
 export const DefaultValue: Story = Template.bind({});
 DefaultValue.args = {
@@ -54,3 +106,11 @@ const StyledTemplate = (args: any) => ({
 });
 
 export const BootstrapStyled: Story = StyledTemplate.bind({});
+BootstrapStyled.args = {
+  type: "text",
+};
+
+export const BootstrapStyledDefaultValue: Story = StyledTemplate.bind({});
+BootstrapStyledDefaultValue.args = {
+  defaultValue: "Default Form Value",
+};
