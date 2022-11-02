4286b442d11cc96d2d27048cff0378e3c9043c6f
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-11-01 21:23:08 -0300

    Add Speech to text wip

---
+++ src/components/SpeechToText/SpeechToText.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+/**
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition
+ */
+import { ref, onMounted } from "vue";
+
+export interface Props {
+  lang?: string; // en-US, pt-BR, ...
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits(["start", "stop", "result"]);
+
+const isRecording = ref<boolean>(false);
+const Recognition =
+  (window as any).SpeechRecognition || (window as any).webkitSpeechRecognition;
+const sr = new Recognition();
+
+onMounted(() => {
+  sr.continuous = true;
+  sr.interimResults = true;
+  props.lang ? (sr.lang = props.lang) : null;
+
+  sr.onstart = () => {
+    emit("start");
+    isRecording.value = true;
+  };
+
+  sr.onend = () => {
+    emit("stop");
+    isRecording.value = false;
+  };
+
+  sr.onresult = (event: any) => {
+    for (let i = 0; i < event.results.length; i++) {
+      const result = event.results[i];
+      if (result.isFinal) emit("result", result);
+    }
+
+    const text = Array.from(event.results)
+      .map((result: any) => result[0])
+      .map((result) => result.transcript)
+      .join("");
+    emit("result", text);
+  };
+});
+</script>
+
+<template>
+  <div class="app">
+    <div v-if="!isRecording" @click="sr.start()"><i class="bi bi-mic"></i></div>
+    <div v-else @click="sr.stop()"><i class="bi bi-mic-mute"></i></div>
+  </div>
+</template>
---
+++ src/components/SpeechToText/docs/SpeechToText.stories.ts
@@ -0,0 +1,35 @@
+import SpeechToText from "@/components/SpeechToText/SpeechToText.vue";
+import type { Meta, Story } from "@storybook/vue3";
+import { actions } from "@storybook/addon-actions";
+
+export default {
+  title: "Components/SpeechToText/SpeechToText",
+  component: SpeechToText,
+  argTypes: {
+    onStart: {},
+    onStop: {},
+    onResult: {},
+  },
+  decorators: [
+    () => ({ template: "<div style='padding: 3em'><story /></div>" }),
+  ],
+} as Meta;
+
+const actionsData = {
+  onStart: actions("start"),
+  onStop: actions("stop"),
+  onResult: actions("result"),
+};
+
+const Template = (args: any) => ({
+  components: { SpeechToText },
+  setup() {
+    return { args };
+  },
+  template: `
+    <SpeechToText v-bind='args'/>
+  `,
+});
+
+export const Default: Story = Template.bind({});
+Default.args = {};
